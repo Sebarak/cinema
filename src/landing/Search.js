04338f238254d5e4b34d254123dae08ddd737a35
unknown <sebastian.rak9@gmail.com>
2022-04-13 03:50:06 +0200

    Create search section

--- src/landing/Search.js
+++ src/landing/Search.js
@@ -1,7 +1,12 @@
-const Search = () =>(
-    <section className="container search">
-
-    </section>
-)
+const Search = () =>{
+    return(
+        <section className="container search">
+            <div className='searching'>
+                <input type="text" className='searching_input' placeholder='np. Avengers'/>
+                <button className='searching_button'><i className="fa-solid fa-magnifying-glass" /></button>
+            </div>
+        </section>
+    )
+}
 
 export {Search}
